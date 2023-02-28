f6fb116100884c5a3855faa378dfa4c2bbe467d2
Lucas <lucasvmorales@protonmail.com>
2023-02-28 08:59:32 -0300

    (feat): Creating first phpunit test

---
+++ teste-avaliador.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+
+$leilao = new Leilao('Fiat 147 0KM');
+
+$maria = new Usuario('Maria');
+$joao = new Usuario('João');
+
+$leilao->recebeLance(new Lance($joao, 2000));
+$leilao->recebeLance(new Lance($maria, 2500));
+
+$leiloeiro = new Avaliador();
+$leiloeiro->avalia($leilao);
+
+$maiorValor = $leiloeiro->getMaiorValor();
+
+echo "O maior lance foi de R$ {$maiorValor}";  

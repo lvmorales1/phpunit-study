b742a2d79f82ff3f66fa4a1d0aa7fcc85167c0ef
Lucas <lucasvmorales@protonmail.com>
2023-02-28 09:04:03 -0300

    (doc): Improving code visibility

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -7,6 +7,7 @@ use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Model\Usuario;
 use Alura\Leilao\Service\Avaliador;
 
+// Arrumo o teste para que ele passe
 $leilao = new Leilao('Fiat 147 0KM');
 
 $maria = new Usuario('Maria');
@@ -15,12 +16,16 @@ $joao = new Usuario('João');
 $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
+// Executo o código a ser testado
 $leiloeiro = new Avaliador();
 $leiloeiro->avalia($leilao);
 
+// Verifico se o resultado é o esperado
+$valorEsperado = 2500;
+
 $maiorValor = $leiloeiro->getMaiorValor();
 
-if ($maiorValor == 2500) {
+if ($maiorValor == $valorEsperado) {
     echo "TESTE OK";
 } else {
     echo "TESTE FALHOU";
